ae0bb3c8f022baf59d1d7882512b1cd077d01bd6
Jeremy Nelson <jermnelson.gmail.com>
2014-09-15 15:38:31 -0600

    updated RDA enhancement

---
+++ rda_enhancement/__init__.py
@@ -0,0 +1,7 @@
+__version_info__ = ('0', '0', '1')
+__version__ = '.'.join(__version_info__)
+__author__ = "Jeremy Nelson"
+__license__ = 'MIT'
+__copyright__ = '(c) 2014 by Jeremy Nelson, Colorado College'
+
+import tests as tests
